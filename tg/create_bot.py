3de542cdcc1aea101515254f71a968456012e271
MrMegnis <mr.megnis@mail.ru>
2023-03-19 23:43:13 +0500

    remove token

--- tg/create_bot.py
+++ tg/create_bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from token import token
 
-bot = Bot(token="6134726581:AAFqn92pAgeGKQo8lxzEB498cOPBS0M0XVo")
+bot = Bot(token=token)
 dispatcher = Dispatcher(bot)
