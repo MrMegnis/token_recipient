96012b7d36251fa3de8808860883a2170741a697
MrMegnis <mr.megnis@mail.ru>
2023-03-20 20:43:18 +0500

    test deploy

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
 from tg import bot
+import schedule
+import requests
+
+
+def wake_up():
+    response = requests.get("https://glitch.com/~crystalline-flint-grease")
+    print("waked up!")
+
 
 bot.start_bot()
+schedule.every(5).minute(wake_up)
