cc8dde85f4f5d238bf35711d4c1ac2def38f28a8
MrMegnis <mr.megnis@mail.ru>
2023-03-20 19:01:19 +0500

    test deploy

--- tg/create_bot.py
+++ tg/create_bot.py
@@ -2,6 +2,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from token import token
+import os
 
-bot = Bot(token=token)
+bot = Bot(token=os.getenv("BOTTOKEN"))
 dispatcher = Dispatcher(bot)
