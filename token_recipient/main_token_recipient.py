99284fc60ea38c47761f280631429a1c570bf53d
MrMegnis <97959994+MrMegnis@users.noreply.github.com>
2023-03-30 18:26:56 +0500

    Update main_token_recipient.py

--- token_recipient/main_token_recipient.py
+++ token_recipient/main_token_recipient.py
@@ -1,18 +1,26 @@
 from dotenv import load_dotenv
 from flask import Flask
 import token_recipient
+import sys
+import os
+sys.path.append('../app')
+directory = os.getcwd()
+
+print(directory)
+print(os.listdir())
 
 from data import db_session
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mega_secret_key'
 
-
+@app.route('/')
+def index():
+  return "aboba"
 
 
 if __name__ == "__main__":
     load_dotenv('.env')
     db_session.global_init()
     app.register_blueprint(token_recipient.blueprint)
-    app.run(host="0.0.0.0", port=3001, debug=True)
+    app.run()
